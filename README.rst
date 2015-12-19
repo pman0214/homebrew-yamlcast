a13e6ff5784f315306b035989d5a4aef8da931db
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2015-12-19 12:34:19 +0900

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,14 @@
+===================
+ homebrew-yamlcast
+===================
+
+`Yamlcast <https://github.com/unnonouno/yamlcast>`_ is a C++ library handling YAML files.
+This tap helps to install yamlcast onto OSX >= 10.9.
+
+How to use
+==========
+
+.. code-block:: bash
+
+   % brew tap pman0214/yamlcast
+   % brew install yamlcast
---
+++ yamlcast.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Yamlcast < Formula
+  homepage 'http://yamlcast.readthedocs.org/'
+  head 'https://github.com/unnonouno/yamlcast.git'
+  sha1 'b3be97c0a87ae8cf79228d751e4a36bdee4013af'
+  version 'mavericks-2015219'
+
+  depends_on 'libyaml'
+  depends_on 'pficommon'
+  depends_on 'pkg-config'
+
+  def install
+    if MacOS.version >= "10.9"
+      ENV['CXXFLAGS'] = '-std=c++11'
+    end
+    system './waf', 'configure', "--prefix=#{prefix}"
+    system './waf'
+    system './waf', 'install'
+  end
+end
